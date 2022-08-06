216f5f0fa422f3edc11b551b903443565a06bfeb
Elizeu <elizeu_os@hotmail.com>
2022-08-06 16:29:22 -0300

    Add styles to header

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,29 +1,40 @@
-import './header.scss';
+import Search from "../search/search";
+import "./header.scss";
 
-function Header(){
-    return(
-    <header>
-        <div className= "c-container">
-            <div className='l-flex l-flex--center l-flex--wrap l-flex--negative'>
+const logo = "/favicon.png";
 
-                <article class="l__col-4">
-                    <div className="c-show-side-bar">
-                            asdsd
-                    </div>
+function Header() {
+  return (
+    <header className="c-header">
+      <div className="c-container">
+        <div className="l-flex l-flex--center l-flex--wrap l-flex--negative">
+          <article className="l__col-4">
+            <div className="l-flex l-flex--center">
 
-                    <h1>
-                        Agenda VExpenses
-                        </h1>
-                </article>
+              <div className="c-header__show-sidebar">
+              <div className="c-header__show-sidebar__dash">
+                </div>
+              </div>
 
-                <article class="l__col-8">
-                    <input type="text" placeholder='Pesquisa'/>
-                </article>
+              <a href="/" className="c-header__logo">
+                <img
+                  src={logo}
+                  className="c-header__image"
+                  alt="Agenda VExpenses"
+                />
+              </a>
 
+              <h1>Agenda VExpenses</h1>
             </div>
+          </article>
+
+          <article className="l__col-8">
+            <Search />
+          </article>
         </div>
+      </div>
     </header>
-    );
+  );
 }
 
 export default Header;
---
+++ src/components/search/search.js
@@ -0,0 +1,9 @@
+function Search(){
+    return(
+    <>
+    <input type="text" placeholder="Pesquisa" />
+    </>
+    );
+}
+
+export default Search;
