92222f67b808113a8ba8e3a6578a80b4438d8920
Elizeu <elizeu_os@hotmail.com>
2022-08-06 14:38:50 -0300

    Create home page

---
+++ src/pages/home/home.js
@@ -0,0 +1,25 @@
+function Home(){
+    return(<>
+     <div className= "c-container">
+            <div className='l-flex l-flex--center l-flex--wrap l-flex--negative'>
+
+                <article class="l__col-4">
+                    <div className="c-show-side-bar">
+                            asdsd
+                    </div>
+
+                    <h1>
+                        Agenda VExpenses
+                        </h1>
+                </article>
+
+                <article class="l__col-8">
+                    <input type="text" placeholder='Pesquisa'/>
+                </article>
+
+            </div>
+        </div>
+    </>);
+}
+
+export default Home;
