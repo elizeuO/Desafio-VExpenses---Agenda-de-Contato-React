9ddeaa2bbb1d4b7dfdb5a84d30bbf6126c450d7a
Elizeu <elizeu_os@hotmail.com>
2022-08-08 23:49:23 -0300

    Add search component

--- src/components/search/search.js
+++ src/components/search/search.js
@@ -1,9 +1,25 @@
-function Search(){
-    return(
+import './search.scss';
+import { FaSearch } from "react-icons/fa";
+import { useState } from 'react';
+
+function Search() {
+    const [focused, SetFocused] = useState(false);
+    const classValues= focused ? 'c-search active' : 'c-search';
+
+  return (
     <>
-    <input type="text" placeholder="Pesquisa" />
+      <article className="l__col-8">
+        <div className={classValues} tabIndex="0">
+            <FaSearch/>
+        <input type="text" placeholder="Pesquisa" onFocus={()=>{
+             SetFocused(true);
+        }} onBlur={()=>{
+            SetFocused(false);
+        }}/>
+        </div>
+      </article>
     </>
-    );
+  );
 }
 
 export default Search;
