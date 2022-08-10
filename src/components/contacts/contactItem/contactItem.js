0d3dc8633943854107e22d6280104eab17801aa3
Elizeu <elizeu_os@hotmail.com>
2022-08-10 03:30:47 -0300

    Add search  bar features

--- src/components/contacts/contactItem/contactItem.js
+++ src/components/contacts/contactItem/contactItem.js
@@ -1,11 +1,13 @@
 import { Link } from "react-router-dom";
 import "./contactItem.scss";
 
-function ContactItem({ id, name, phone, email, photo }) {
-  const contactLink = "/contato/?id="+id;
+function ContactItem({ id, name, phone, email, photo, setIsTyping, isSearch }) {
+  const contactLink = "/contato/?id=" + id;
 
   return (
-    <Link to={contactLink} className="c-contact-item">
+    <Link to={contactLink} className="c-contact-item" onClick={()=>{
+      setIsTyping(false);
+    }}>
       <div className="l-flex l-flex--center l-flex--wrap l-flex--negative">
         <article className="l__col-4">
           <img src={photo} className="c-contact-item__photo" />
@@ -14,7 +16,7 @@ function ContactItem({ id, name, phone, email, photo }) {
 
         <article className="l__col-4">{phone}</article>
 
-        <article className="l__col-4">{email}</article>
+        {!isSearch && <article className="l__col-4">{email}</article>}
       </div>
     </Link>
   );
--- src/components/search/search.js
+++ src/components/search/search.js
@@ -1,21 +1,119 @@
-import './search.scss';
+import "./search.scss";
 import { FaSearch } from "react-icons/fa";
-import { useState } from 'react';
+import { useState } from "react";
+import ContactItem from "../contacts/contactItem/contactItem";
+
+// Add exception for lint
+/* global gapi   google*/
 
 function Search() {
-    const [focused, SetFocused] = useState(false);
-    const classValues= focused ? 'c-search active' : 'c-search';
+  const [focused, SetFocused] = useState(false);
+  const [isTyping, setIsTyping] = useState(false);
+  const [searchResults, setSearchResults] = useState(false);
+  const classValues = focused ? "c-search active" : "c-search";
+
+  async function searchContacts(ev) {
+    const query = ev.target.value;
+
+    if ("" === query) {
+      return;
+    }
+
+    let response;
+    try {
+      response = await gapi.client.people.people.searchContacts({
+        query: query,
+        pageSize: 10,
+        readMask: "names,phoneNumbers,emailAddresses,photos",
+      });
+    } catch (err) {
+      console.log("Error searching contacts");
+      return;
+    }
+
+    const results = response.result.results;
+
+    renderSearchResults(formatConnectionsResult(results));
+  }
+
+  function formatConnectionsResult(connections) {
+    if (undefined === connections) {
+      setSearchResults("Nenhum resultado foi encontrado");
+      return;
+    }
+    const result = connections.map((person) => {
+      const name = person.person.names[0].displayName;
+      const photo = person.person.photos[0].url;
+      const id = person.person.resourceName.split("people/")[1];
+
+      const data = { id: id, name, photo: photo };
+
+      if (undefined !== person.person.phoneNumbers) {
+        const phone = person.person.phoneNumbers[0].value;
+        data.phone = phone;
+      }
+
+      if (undefined !== person.person.emailAddresses) {
+        const email = person.person.emailAddresses[0].value;
+        data.email = email;
+      }
+
+      return data;
+    });
+
+    return sortAlphabetically(result);
+  }
+
+  function sortAlphabetically(array) {
+    const result = array.sort(function (a, b) {
+      if (a.name < b.name) {
+        return -1;
+      }
+      if (a.name > b.name) {
+        return 1;
+      }
+      return 0;
+    });
+
+    return result;
+  }
+
+  function renderSearchResults(results) {
+    if (results === undefined) {
+      return;
+    }
+
+    const content = results.map((result) => {
+      return (
+        <ContactItem
+          id={result.id}
+          name={result.name}
+          phone={result.phone}
+          photo={result.photo}
+          setIsTyping={setIsTyping}
+          isSearch="true"
+        />
+      );
+    });
+    setIsTyping(true);
+    setSearchResults(content);
+  }
 
   return (
     <>
       <article className="l__col-8">
-        <div className={classValues} tabIndex="0">
-            <FaSearch/>
-        <input type="text" placeholder="Pesquisa" onFocus={()=>{
-             SetFocused(true);
-        }} onBlur={()=>{
-            SetFocused(false);
-        }}/>
+        <div className={classValues}>
+          <FaSearch />
+          <input
+            type="text"
+            placeholder="Pesquisa"
+            onChange={searchContacts}
+            onBlur={() => {
+              SetFocused(false);
+            }}
+          />
+
+          {isTyping && <div className="c-search__results">{searchResults}</div>}
         </div>
       </article>
     </>
